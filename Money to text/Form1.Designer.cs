b4b58ae01d6e6e611f795caa64cc3ea49d7c421c
Teheft1 <yunus.22081@mhs.unesa.ac.id>
2023-11-01 21:02:48 +0700

    Add project files.

---
+++ Money to text/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Money_to_text
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MoneyIN = new System.Windows.Forms.TextBox();
+            this.TextConvert = new System.Windows.Forms.TextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MoneyIN
+            // 
+            this.MoneyIN.Location = new System.Drawing.Point(161, 144);
+            this.MoneyIN.Name = "MoneyIN";
+            this.MoneyIN.Size = new System.Drawing.Size(100, 20);
+            this.MoneyIN.TabIndex = 0;
+            this.MoneyIN.TextChanged += new System.EventHandler(this.MoneyIN_TextChanged);
+            // 
+            // TextConvert
+            // 
+            this.TextConvert.Location = new System.Drawing.Point(161, 189);
+            this.TextConvert.Name = "TextConvert";
+            this.TextConvert.Size = new System.Drawing.Size(296, 20);
+            this.TextConvert.TabIndex = 1;
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(336, 160);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(75, 23);
+            this.Convert.TabIndex = 2;
+            this.Convert.Text = "button1";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(632, 461);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.TextConvert);
+            this.Controls.Add(this.MoneyIN);
+            this.Name = "Form1";
+            this.Text = "Money To Text Convert";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MoneyIN;
+        private System.Windows.Forms.TextBox TextConvert;
+        private System.Windows.Forms.Button Convert;
+    }
+}
+
---
+++ Money to text/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Money_to_text
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static string MoneyToText(int number)
+        {
+            string Words = "";
+
+            if (number == 0) return "zero";
+
+            if ((number / 1000000) > 0)
+                Words += MoneyToText(number / 1000000) + " million ";
+                number %= 1000000;
+
+            if ((number / 1000) > 0)
+                Words += MoneyToText(number / 1000) + " Thousand ";
+                number %= 1000;
+
+            if ((number / 100) > 0)
+                Words += MoneyToText(number / 100) + " Hundred ";
+                number %= 100;
+
+            if (number > 0)
+                if (Words != "") Words += " and ";
+                var unitsMap = new[] { "zero", "one","two", "three", "four", "five", "six","seven","eight", "nine", "ten","eleven","twelve","thirteen", "fourteen", "fifteen","sixteen","seventeen","eighteen","nineteen" };
+                var tensmap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty","sixty", "seventy", "eighty", "ninety"};
+                
+                if (number < 20)
+                {
+                    Words += unitsMap[number];
+                }
+                 else
+                {
+                    Words += tensmap[number / 10];
+                if (number % 10 > 0)
+                    Words += " " + unitsMap[number % 10];
+                }
+
+
+
+
+            return Words;
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            TextConvert.Text = MoneyToText(int.Parse(MoneyIN.Text));
+        }
+
+        private void MoneyIN_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
